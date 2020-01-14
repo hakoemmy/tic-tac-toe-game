a63d5bcc840b7c939bed0b9892ee363065295b69
Emmanuel HAKORIMANA <hakoemmy.2017@gmail.com>
2020-01-14 09:09:03 +0200

    Updates

---
+++ precache-manifest.a49c616658bf575c3811245c375cfd3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4858bb9e16d086bbb821ca0c45043a47",
+    "url": "/tic-tac-toe-game/index.html"
+  },
+  {
+    "revision": "59471b3ff6b037544c4b",
+    "url": "/tic-tac-toe-game/static/css/main.d4bd7d3d.chunk.css"
+  },
+  {
+    "revision": "4124e40a7881347275fc",
+    "url": "/tic-tac-toe-game/static/js/2.70fb31b7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tic-tac-toe-game/static/js/2.70fb31b7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "59471b3ff6b037544c4b",
+    "url": "/tic-tac-toe-game/static/js/main.29f65d92.chunk.js"
+  },
+  {
+    "revision": "ed15c95b56814cac30a6",
+    "url": "/tic-tac-toe-game/static/js/runtime-main.1a2e01ce.js"
+  }
+]);
